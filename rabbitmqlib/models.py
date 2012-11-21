2bd78c1153fe4e802f1df74d9c2b3c276750f042
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2012-11-21 14:45:52 +0100

    pep8

--- rabbitmqlib/models.py
+++ rabbitmqlib/models.py
@@ -8,8 +8,11 @@ class Rabbit:
 
     def __init__(self, server, user, password, vhost):
         credentials = pika.PlainCredentials(user, password)
-        conn_params = pika.ConnectionParameters(server, virtual_host = vhost,
-            credentials = credentials)
+        conn_params = pika.ConnectionParameters(
+            server,
+            virtual_host=vhost,
+            credentials=credentials
+        )
         conn_broker = pika.BlockingConnection(conn_params)
         self.channel = conn_broker.channel()
 
@@ -19,24 +22,38 @@ class Producer(Rabbit):
     def send(self, msg, exchange, routing_key):
         queue = b"%s_%s" % (exchange, routing_key)
         ch = self.channel
-        ch.exchange_declare(exchange=exchange,
-            auto_delete=False)
-        ch.queue_declare(queue=queue)
-        ch.queue_bind(queue=queue, exchange=exchange, routing_key=routing_key)
-        ch.basic_publish(body = json.dumps(msg),
-            exchange = exchange, routing_key=routing_key)
+        ch.exchange_declare(
+            exchange=exchange,
+            auto_delete=False
+        )
+        ch.queue_declare(
+            queue=queue
+        )
+        ch.queue_bind(
+            queue=queue,
+            exchange=exchange,
+            routing_key=routing_key
+        )
+        ch.basic_publish(
+            body=json.dumps(msg),
+            exchange=exchange,
+            routing_key=routing_key
+        )
 
 
 class Consumer(Rabbit):
-    
+
     def queue_name(self):
         return ""
 
     def consume(self):
         ch = self.channel
         ch.queue_declare(queue=self.queue_name())
-        ch.basic_consume(self._callback, queue=self.queue_name(),
-            consumer_tag=self.__class__.__name__)
+        ch.basic_consume(
+            self._callback,
+            queue=self.queue_name(),
+            consumer_tag=self.__class__.__name__
+        )
         ch.start_consuming()
 
     def callback(self, body):
@@ -45,4 +62,3 @@ class Consumer(Rabbit):
     def _callback(self, channel, method, header, body):
         self.callback(body)
         channel.basic_ack(delivery_tag=method.delivery_tag)
-
